f9ea57fc2384a3b5a7859be57aac0a7bfca1b0dc
Naïké Dramé <naike.drame.dev@gmail.com>
2025-02-14 10:11:47 +0100

    intro MVC & demo controller

---
+++ BookingManager.MVC/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingManager.MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Hello([FromQuery] string name)
+        {
+            return $"Hello {name}!!!";
+        }
+
+        public string Pause()
+        {
+            #region Solution proposée
+            //DateTime time = DateTime.Now;
+            //DateTime breakTime = DateTime.Today.AddHours(10);
+            //TimeSpan diff = breakTime - time;
+            //string msg = time < breakTime ? $"Il reste {diff.Minutes}min{diff.Seconds}s avant la pause." : "C'est la pause!";
+
+            //return msg; 
+            #endregion
+
+            #region Correction
+            int heurePause = 10;
+            if (DateTime.Now.Hour >= heurePause) return "Ce n'est pas encore l'heure de la pause";
+            int minutesRestantes = ((heurePause - DateTime.Now.Hour - 1) * 60) + (60 - DateTime.Now.Minute);
+            return $"Il reste {minutesRestantes} minutes avant la pause.";
+            #endregion
+        }
+    }
+}
---
+++ BookingManager.MVC/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline = middlewares.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// on peut ajouter des routes ici
+// comme les middlewares s'exécutent les uns après les autres, et comme le programme va trouver une réponse, on ne verra pas Hello World mais Machin
+// app.MapGet("/test/hello", () => "Machin");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
